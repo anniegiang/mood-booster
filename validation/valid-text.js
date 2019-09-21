9fc0f52daeedec6a582f6806392a9dc3404fcd8a
joonson22 <johnsonlai22@gmail.com>
2019-09-21 16:20:13 -0700

    set up auth input validations

---
+++ validation/valid-text.js
@@ -0,0 +1,6 @@
+// check whether a given string consists of valid input
+const validText = str => {
+  return typeof str === "string" && str.trim().length > 0; // trim removes whitespace
+};
+
+module.exports = validText;
