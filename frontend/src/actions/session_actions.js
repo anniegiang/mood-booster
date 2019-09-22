b3397cb35848b26ce95601f177157d0b2f095eec
joonson22 <johnsonlai22@gmail.com>
2019-09-22 15:29:35 -0700

    create auth components

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -7,38 +7,36 @@ export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
 
 // We'll dispatch this when our user signs in
-const receiveCurrentUser = currentUser => ({
+export const receiveCurrentUser = currentUser => ({
   type: RECEIVE_CURRENT_USER,
   currentUser
 });
 
 // This will be used to redirect the user to the login page upon signup
-const receiveUserSignIn = () => ({
+export const receiveUserSignIn = () => ({
   type: RECEIVE_USER_SIGN_IN
 });
 
-// When our user is logged out, we will dispatch this action to set isAuthenticated to false
-const logoutUser = () => ({
-  type: RECEIVE_USER_LOGOUT
-});
-
 // We dispatch this one to show authentication errors on the frontend
-const receiveErrors = errors => ({
+export const receiveErrors = errors => ({
   type: RECEIVE_SESSION_ERRORS,
   errors
 });
 
-// thunk
+// When our user is logged out, we will dispatch this action to set isAuthenticated to false
+export const logoutUser = () => ({
+  type: RECEIVE_USER_LOGOUT
+});
 
 // Upon signup, dispatch the approporiate action depending on which type of response we receieve from the backend
-export const signup = user => dispatch => {
+export const signup = user => dispatch =>
   APIUtil.signup(user).then(
     () => dispatch(receiveUserSignIn()),
     err => dispatch(receiveErrors(err.response.data))
   );
-};
 
-export const login = user => dispatch => {
+// Upon login, set the session token and dispatch the current user. Dispatch errors on failure.
+export const login = user => dispatch =>
   APIUtil.login(user)
     .then(res => {
       const { token } = res.data;
@@ -47,9 +45,11 @@ export const login = user => dispatch => {
       const decoded = jwt_decode(token);
       dispatch(receiveCurrentUser(decoded));
     })
-    .catch(err => receiveErrors(err.response.data));
-};
+    .catch(err => {
+      dispatch(receiveErrors(err.response.data));
+    });
 
+// We wrote this one earlier
 export const logout = () => dispatch => {
   localStorage.removeItem("jwtToken");
   APIUtil.setAuthToken(false);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -25,6 +25,8 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     store = configureStore({});
   }
+
+  window.store = store;
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
 });
