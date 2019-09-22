2e16daab2d1abf2e20e204b34c6039d5a5b37822
joonson22 <johnsonlai22@gmail.com>
2019-09-21 17:49:30 -0700

    create tweets validation

--- models/User.js
+++ models/User.js
@@ -1,27 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    // create props
-    handle: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+  // create props
+  handle: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-// connect to mongoose by passing in schema 
-User = mongoose.model('users', UserSchema);
+// connect to mongoose by passing in schema
+const User = mongoose.model("users", UserSchema);
 
 module.exports = User;
