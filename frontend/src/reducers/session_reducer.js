a6160e163515c5fac981ad89e58fc57b51956467
joonson22 <johnsonlai22@gmail.com>
2019-09-22 14:32:59 -0700

    finish session reducers

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,4 +1,8 @@
-import { RECEIVE_USER_LOGOUT } from "../actions/session_actions";
+import {
+  RECEIVE_USER_LOGOUT,
+  RECEIVE_USER_SIGN_IN,
+  RECEIVE_CURRENT_USER,
+} from "../actions/session_actions";
 
 const initialState = {
   isAuthenticated: false,
@@ -12,6 +16,17 @@ const sessionReducer = (state = initialState, action) => {
       return {
         isAuthenticated: false,
         user: undefined
+      };
+    case RECEIVE_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !!action.currentUser,
+        user: action.currentUser
+      };
+    case RECEIVE_USER_SIGN_IN:
+      return {
+        ...state,
+        isSignedIn: true
       };
     default:
       return state;
