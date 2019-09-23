0cbb2163f79646e3c73572ad534e57734955a4a0
Johnson <joonson22@gmail.com>
2019-09-23 14:50:10 -0700

    Created videos model and started video routes

---
+++ models/Video.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const VideoSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    videoUrl: {
+        type: String,
+        required: true
+    },
+    mood1: {
+        type: Boolean,
+        required: true
+    },
+    mood2: {
+        type: Boolean,
+        required: true
+    },
+    mood3: {
+        type: Boolean,
+        required: true
+    },
+    mood4: {
+        type: Boolean,
+        required: true
+    },
+    mood5: {
+        type: Boolean,
+        required: true
+    },
+    mood6: {
+        type: Boolean,
+        required: true
+    },
+    mood7: {
+        type: Boolean,
+        required: true
+    },
+})
+
+module.exports = Video = mongoose.model('videos', VideoSchema)
