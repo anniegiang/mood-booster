a65d951646686a23bb008e76a12c24e6039da743
joonson22 <johnsonlai22@gmail.com>
2019-09-25 07:31:23 -0700

    quote can save

--- frontend/src/components/quotes/quote.js
+++ frontend/src/components/quotes/quote.js
@@ -3,11 +3,11 @@ import React from "react";
 class Quote extends React.Component {
   constructor(props) {
     super(props);
-    // this.id = this.props.quoteID 
+    // this.id = this.props.quoteID
 
-//    this.text = this.props.text;
-//    this.author = this.props.author;
-    this.saveQuote = this.saveQuote.bind(this)
+    //    this.text = this.props.text;
+    //    this.author = this.props.author;
+    this.saveQuote = this.saveQuote.bind(this);
   }
 
   componentDidMount() {
@@ -16,26 +16,28 @@ class Quote extends React.Component {
 
   saveQuote(e) {
     e.preventDefault();
-    // this.props.composeTweet({ text: this.state.text });
-    // this.setState({ text: "" });
+    let data = {
+      contentType: "quoteSave",
+      contentId: this.props.match.params.quote_id,
+      userId: this.props.currentUser.id
+    };
+    debugger;
+    this.props.saveContent(data);
   }
-
 
   render() {
     if (!this.props.quotes) {
-      return null
-  }
-      return (
-        <div>
-          <h1>This is a quote</h1>
-            <h3>{this.props.quotes.quoteText}</h3>
-            <h5>{this.props.quotes.author}</h5>
-            {/* <p>Quote will go here</p> */}
-            <button onClick={this.saveQuote}>Save to Favorites</button>
-        </div>
-
-      );
-    
+      return null;
+    }
+    return (
+      <div>
+        <h1>This is a quote</h1>
+        <h3>{this.props.quotes.quoteText}</h3>
+        <h5>{this.props.quotes.author}</h5>
+        {/* <p>Quote will go here</p> */}
+        <button onClick={this.saveQuote}>Save to Favorites</button>
+      </div>
+    );
   }
 }
 
