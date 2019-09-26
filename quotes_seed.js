af1669bb56b6a0fffeb36fe5d0aede5d6918f3a9
joonson22 <johnsonlai22@gmail.com>
2019-09-25 22:36:40 -0700

    rename seed files

--- quotes_seed.js
+++ quotes_seed.js
@@ -1,30 +1,47 @@
 const Quote = require("./models/Quote");
 const seed2 = () => {
-    const quote2 = new Quote({
-        "title": "Quote Title",
-        "quoteText": "This is the quote",
-        "author": "me",
-        "mood1": false,
-        "mood2": false,
-        "mood3": false,
-        "mood4": false,
-        "mood5": false,
-        "mood6": false,
-        "mood7": false
-    })
-    const quote3 = new Quote({
-        "title": "2nd Quote Title",
-        "quoteText": "This is the second quote",
-        "author": "johnson",
-        "mood1": false,
-        "mood2": true,
-        "mood3": false,
-        "mood4": false,
-        "mood5": false,
-        "mood6": false,
-        "mood7": false
-    })
-    quote3.save()
-}
+  const quote1 = new Quote({
+    title: "Quote 1",
+    quoteText:
+      "Don't let the fear of losing be greater than the excitement of winning.",
+    author: "Robert Kiyosaki",
+    mood1: false,
+    mood2: false,
+    mood3: false,
+    mood4: false,
+    mood5: false,
+    mood6: false,
+    mood7: false
+  });
+  quote1.save();
+  const quote2 = new Quote({
+    title: "Quote 2",
+    quoteText: "Always remember that the future comes one day at a time.",
+    author: "Dean Acheson",
+    mood1: false,
+    mood2: false,
+    mood3: false,
+    mood4: false,
+    mood5: false,
+    mood6: false,
+    mood7: false
+  });
 
-module.exports = seed2
+  quote2.save();
+
+  const quote3 = new Quote({
+    title: "Quote 3",
+    quoteText: "If you cannot do great things, do small things in a great way.",
+    author: "Napoleon Hill",
+    mood1: false,
+    mood2: true,
+    mood3: false,
+    mood4: false,
+    mood5: false,
+    mood6: false,
+    mood7: false
+  });
+  quote3.save();
+};
+
+module.exports = seed2;
