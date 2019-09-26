deefa19d412a95495d0b37c629e72651d92eba2c
joonson22 <johnsonlai22@gmail.com>
2019-09-26 08:30:15 -0700

    Add logo

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -7,7 +7,7 @@ class MainPage extends React.Component {
       <div className="mainpage-container">
         <div className="splash-window">
           <h2>Welcome to</h2>
-          <h1>Mood Booster</h1>
+          <div className="logo-main"></div>
           <p>Being in tech can be a grueling experience where individuals can learn a lot, but still feel like they don't understand anything. 
             Drawing from our experiences, we learned that productivity is directly correlated to an indivdual's mood, and so we've set out to create a way to boost that mood and, 
             hopefully, overall performance. Click the button below to check it out!</p>
