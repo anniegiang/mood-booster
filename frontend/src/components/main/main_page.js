710c1b685505df5aa6c8e56fae188f2f98504ca5
joonson22 <johnsonlai22@gmail.com>
2019-09-26 07:35:52 -0700

    Design splash page, add media query

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -5,8 +5,14 @@ class MainPage extends React.Component {
   render() {
     return (
       <div className="mainpage-container">
-        <h1>Mood Booster</h1>
-        <button>Boost your mood</button>
+        <div className="splash-window">
+          <h2>Welcome to</h2>
+          <h1>Mood Booster</h1>
+          <p>Being in tech can be a grueling experience where individuals can learn a lot, but still feel like they don't understand anything. 
+            Drawing from our experiences, we learned that productivity is directly correlated to an indivdual's mood, and so we've set out to create a way to boost that mood and, 
+            hopefully, overall performance. Click the button below to check it out!</p>
+          <button className="main_button">Boost your mood!</button>
+        </div>
       </div>
     );
   }
