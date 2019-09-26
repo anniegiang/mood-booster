3fd87f3a9322981f3110114440be0e44391e02b8
Rebecca Lambert <rebeccalambert@berkeley.edu>
2019-09-26 09:29:22 -0700

    Prevent errors from moving input boxes on login/signup, Create 'about' page

---
+++ frontend/src/components/main/about_page.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./about_page.css";
+
+class MainPage extends React.Component {
+  render() {
+    return (
+      <div className="mainpage-container">
+        <div className="splash-window">
+          <h2>Welcome to</h2>
+          <div className="logo-main"></div>
+          <p>Being in tech can be a grueling experience where individuals learn a lot, but still feel like they don't understand anything. 
+            Drawing from our experiences, we learned that productivity is directly correlated to an individual's mood, and so we've set out to create a way to boost that mood and, 
+            hopefully, overall performance. Click the button below to check it out!</p>
+          <button className="main_button">Boost your mood!</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
