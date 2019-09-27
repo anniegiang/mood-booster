14b019667e04c2fc8e716b56848e584fb45f0200
Johnson <joonson22@gmail.com>
2019-09-27 11:09:08 -0700

    Added conditional in routes to check for user input of videos/photos/quotes

--- frontend/src/components/search/search.js
+++ frontend/src/components/search/search.js
@@ -2,50 +2,60 @@ import React from 'react';
 
 class Search extends React.Component {
     componentDidMount(){
-        if (this.props.location.state.videos) {
+        // USE IF STATEMENT TO KEEP IN GLOBAL STATE GOING TO NEXT PAGE IF YOU WANT TO SHOW SAME VIDEOS WHEN THEY PRESS BACK BUTTON ON SHOW PAGE
+        // if (this.props.location.state.videos) {
             this.props.fetchVideos(this.props.location.search)
-        }
+        // }
 
-        if (this.props.location.state.photos) {
+        // if (this.props.location.state.photos) {
             this.props.fetchPhotos(this.props.location.search)
-        }
+        // }
 
-        if (this.props.location.state.quotes) {
+        // if (this.props.location.state.quotes) {
             this.props.fetchQuotes(this.props.location.search)
-        }
+        // }
     }
     render() {
-    //    debugger
+     
         if (!this.props.videos) {
             return null
         }
-        return(
-            
-            <div>
-                {this.props.videos ? (
-                    this.props.videos.map(video => {
-                    return <li>{video.title}</li>
-                    })
+        if (this.props.videos.length === 0 && this.props.photos.length === 0 && this.props.quotes.length === 0) {
+            return (
+                <div>
+                    <h1>Sorry no Content!! Go to form page ya mankey</h1>
+                </div>
+            )
+        } else {
+            return(
+                
+                <div>
                     
-                ): ("")
-                }
-
-                {this.props.photos ? (
-                    this.props.photos.map(photo => {
-                    return <li>{photo.title}</li>
-                    })
-                ): ("")
-                }
-
-                {this.props.quotes ? (
-                    this.props.quotes.map(quote => {
-                    return <li>{quote.title}</li>
-                    })
-                ): ("")
-                }
-
-            </div>
-        )
+                    {this.props.videos ? (
+                        this.props.videos.map(video => {
+                        return <li>{video.title}</li>
+                        })
+                        
+                    ): ("")
+                    }
+    
+                    {this.props.photos ? (
+                        this.props.photos.map(photo => {
+                        return <li>{photo.title}</li>
+                        })
+                    ): ("")
+                    }
+    
+                    {this.props.quotes ? (
+                        this.props.quotes.map(quote => {
+                        return <li>{quote.title}</li>
+                        })
+                    ): ("")
+                    }
+    
+                </div>
+            )
+        }
     }
 }
 
--- routes/api/photos.js
+++ routes/api/photos.js
@@ -20,22 +20,22 @@ router.get("/", (req, res) => {
     req.query.mood5 = JSON.parse(req.query.mood5)
     req.query.mood6 = JSON.parse(req.query.mood6)
     req.query.mood7 = JSON.parse(req.query.mood7)
-    // req.query.videos = JSON.parse(req.query.videos)
-    // req.query.photos = JSON.parse(req.query.photos)
-    // req.query.quotes = JSON.parse(req.query.quotes)
+    req.query.photos = JSON.parse(req.query.photos)
+  
 
 
     Photo.find()
         .then(photos => {
             let photoArr = []
             photos.forEach(photo => {
-                if ((photo.mood1 === true && req.query.mood1 === true) ||
-                    (photo.mood2 === true && req.query.mood2 === true) ||
-                    (photo.mood3 === true && req.query.mood3 === true) ||
-                    (photo.mood4 === true && req.query.mood4 === true) ||
-                    (photo.mood5 === true && req.query.mood5 === true) ||
-                    (photo.mood6 === true && req.query.mood6 === true) ||
-                    (photo.mood7 === true && req.query.mood7 === true)) {
+                if ((photo.mood1 === true && req.query.mood1 === true && req.query.photos === true) ||
+                    (photo.mood2 === true && req.query.mood2 === true && req.query.photos === true) ||
+                    (photo.mood3 === true && req.query.mood3 === true && req.query.photos === true) ||
+                    (photo.mood4 === true && req.query.mood4 === true && req.query.photos === true) ||
+                    (photo.mood5 === true && req.query.mood5 === true && req.query.photos === true) ||
+                    (photo.mood6 === true && req.query.mood6 === true && req.query.photos === true) ||
+                    (photo.mood7 === true && req.query.mood7 === true && req.query.photos === true)) 
+                {
                     photoArr.push(photo)
                 }
             })
