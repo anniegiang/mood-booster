7dd0fb3d0d4922c072de5a429cb86009188802ca
joonson22 <johnsonlai22@gmail.com>
2019-09-26 23:28:46 -0700

    render correctly formated random content

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -2,13 +2,41 @@ import React from "react";
 import "./main_page.css";
 
 class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderRandomContentType = this.renderRandomContentType.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.fetchRandomContent();
+  }
+
+  renderRandomContentType() {
+    const { randomContent } = this.props;
+    if ("quoteText" in randomContent) {
+      return <p>{randomContent.quoteText}</p>;
+    }
+
+    if ("videoUrl" in randomContent) {
+      return <video src={randomContent.videoUrl}></video>;
+    }
+
+    if ("photoUrl" in randomContent) {
+      return <img src={randomContent.photoUrl} alt="random-pic" />;
+    }
+  }
+
   render() {
+    if (!this.props.randomContent) {
+      return null;
+    }
+
     return (
       <div className="mainpage-container">
         <div className="content-otd-container">
           <div className="content">
             <h4>Boost of the Day</h4>
-            {this.props.randomContent.title}
+            {this.renderRandomContentType()}
           </div>
         </div>
       </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,29 +4,22 @@ import jwt_decode from "jwt-decode";
 import Root from "./components/root";
 import configureStore from "./store/store";
 import { setAuthToken } from "./util/session_api_util";
+import { fetchUser, saveContent } from "./util/user_api_util";
 import { logout } from "./actions/session_actions";
-import { fetchRandomContent } from "./actions/random_content_actions";
 import axios from "axios";
 
 document.addEventListener("DOMContentLoaded", () => {
   let store;
-  const currentDay = new Date().getDate();
 
   if (localStorage.jwtToken) {
     setAuthToken(localStorage.jwtToken);
     const decodedUser = jwt_decode(localStorage.jwtToken);
     const preloadedState = {
-      session: { isAuthenticated: true, user: decodedUser },
-      content: {
-        randomContent: {
-          content: {},
-          expiresIn: new Date().getDate() + 1 // 24 hours in ms
-        }
-      }
+      session: { isAuthenticated: true, user: decodedUser }
     };
     store = configureStore(preloadedState);
-    const currentTime = Date.now() / 1000;
 
+    const currentTime = Date.now() / 1000;
     if (decodedUser.exp < currentTime) {
       store.dispatch(logout());
       window.location.href = "/login";
@@ -35,15 +28,10 @@ document.addEventListener("DOMContentLoaded", () => {
     store = configureStore({});
   }
 
-  if (
-    store.getState().content.randomContent.expiresIn >= currentDay ||
-    store.getState().content.randomContent.content === {}
-  ) {
-    store.dispatch(fetchRandomContent());
-  }
-
   window.store = store;
   window.axios = axios;
+  window.fetchUser = fetchUser;
+  window.saveContent = saveContent;
 
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
