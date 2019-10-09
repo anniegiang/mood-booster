c74db829798e6a65d018f10f2d2177f1ed6364ef
Annie Giang <annieg4123@gmail.com>
2019-10-09 10:45:16 -0700

    Remove Twitter related files

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -6,14 +6,11 @@ import MainPageContainer from "./main/main_page_container";
 import NavBarContainer from "./nav/navbar_container";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
-import TweetsContainer from "./tweets/tweets_container";
-import TweetComposeContainer from "./tweets/tweet_compose_container";
-import ProfileContainer from "./profile/profile_container";
 import QuoteContainer from "./quotes/quote_container";
 import PhotoContainer from "./photos/photo_container";
-import VideoContainer from './video/video_container';
-import FormContainer  from './form/form_container';
-import SearchContainer from './search/search_container';
+import VideoContainer from "./video/video_container";
+import FormContainer from "./form/form_container";
+import SearchContainer from "./search/search_container";
 import AboutPage from "./main/about_page";
 
 const App = () => (
@@ -23,19 +20,12 @@ const App = () => (
       <Route exact path="/" component={MainPageContainer} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
-      <ProtectedRoute exact path="/profile" component={ProfileContainer} />
-      <ProtectedRoute exact path="/tweets" component={TweetsContainer} />
       <Route exact path="/about" component={AboutPage} />
       <Route exact path="/quotes/:quote_id" component={QuoteContainer} />
       <Route exact path="/photos/:photo_id" component={PhotoContainer} />
       <Route exact path="/form" component={FormContainer} />
       <Route exact path="/search" component={SearchContainer} />
       <Route exact path="/videos/:video_id" component={VideoContainer} />
-      <ProtectedRoute
-        exact
-        path="/new_tweet"
-        component={TweetComposeContainer}
-      />
     </Switch>
   </div>
 );
