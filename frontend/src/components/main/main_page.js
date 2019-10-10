67418c184b9fd8c0c3afc59a6bdb9c5c7e619718
Annie Giang <annieg4123@gmail.com>
2019-10-10 12:39:10 -0700

    style quotes and videos in random content for splash

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -5,42 +5,55 @@ class MainPage extends React.Component {
   constructor(props) {
     super(props);
     this.renderRandomContentType = this.renderRandomContentType.bind(this);
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
     this.props.fetchRandomContent();
     if (this.props.user) {
-      this.props.fetchUser(this.props.user.id)
+      this.props.fetchUser(this.props.user.id);
     }
-  
   }
 
   renderRandomContentType() {
     const { randomContent } = this.props;
     if ("quoteText" in randomContent) {
       return (
-      <span className="main-quote">
-        <p>"{randomContent.quoteText}" </p> 
-        <p>-{randomContent.author}</p>
-      </span>
-      )};
+        <span className="main-quote">
+          <p className="quote-text">"{randomContent.quoteText}" </p>
+          <p className="quote-author">-{randomContent.author}</p>
+        </span>
+      );
+    }
 
     if ("videoUrl" in randomContent) {
       return (
-      <span>
-        <p className="vid_title">{randomContent.title}</p>
-        <video width='426' height='240' src={randomContent.videoUrl} controls></video>
-      </span>
-      )};
+        <span>
+          <p className="vid_title">{randomContent.title}</p>
+          <video
+            width="426"
+            height="240"
+            src={randomContent.videoUrl}
+            controls
+          ></video>
+        </span>
+      );
+    }
 
     if ("photoUrl" in randomContent) {
-      return <img className="main-photo" className="main-photo" src={randomContent.photoUrl} alt="random-pic" />;
+      return (
+        <img
+          className="main-photo"
+          className="main-photo"
+          src={randomContent.photoUrl}
+          alt="random-pic"
+        />
+      );
     }
   }
 
   handleClick() {
-    this.props.history.push({pathname: '/form'})
+    this.props.history.push({ pathname: "/form" });
   }
 
   render() {
@@ -54,9 +67,11 @@ class MainPage extends React.Component {
           <h2>Welcome to</h2>
           <div className="logo-main"></div>
           <p className="random_content">Random Content:</p>
-          
+
           {this.renderRandomContentType()}
-          <button className="main_button" onClick={this.handleClick}>Boost your mood!</button>
+          <button className="main_button" onClick={this.handleClick}>
+            Boost your mood!
+          </button>
         </div>
       </div>
     );
