0d48492068e1caf0a3c0914001e76b0fd2d5796d
Rebecca Lambert <rebeccalambert@berkeley.edu>
2019-10-10 10:43:31 -0700

    Remove debuggers, add key to list elements

--- frontend/src/components/search/search.js
+++ frontend/src/components/search/search.js
@@ -20,7 +20,6 @@ class Search extends React.Component {
     render() {
         
         if (this.props.videos === undefined || this.props.photos === undefined || this.props.quotes === undefined) {
-            // debugger
             return null
         }
         if (this.props.videos.length === 0 && this.props.photos.length === 0 && this.props.quotes.length === 0) {
@@ -30,79 +29,59 @@ class Search extends React.Component {
                 </div>
             )
         } 
-            return(
-                <div className='search-container'>
-                    
 
-                    {/* <div className='result-container'>
-                        {this.props.videos ? (
+        return(
+            <div className='search-container'>
+                
+                {this.props.videos.length > 0 ? (
+                    <div className='result-container'>
+                        {
                             this.props.videos.map(video => {
-                            return <Link to={`/videos/${video._id}`}>
-                                <li className='content'>
-                                    <h1>{video.title}</h1>
-                                    <video controls >
-                                        <source src={video.videoUrl} type="video/mp4"></source>
-                                    </video>                     
+                            return <Link to={`/videos/${video._id}`} key={video._id}>
+                                    <li className='content' >
+                                        <h1>{video.title}</h1>
+                                        <video controls >
+                                            <source src={video.videoUrl} type="video/mp4"></source>
+                                        </video>                     
+                                    </li>
+                                </Link>
+                                })               
+                            }
+                    </div>) : ("")
+                }
+                
+                {this.props.photos.length > 0 ? (
+                    <div className='result-container'>
+                        {this.props.photos.map(photo => {
+                            return <Link to={`/photos/${photo._id}`} key={photo._id}>
+                                <li className='content' >
+                                    <h1>{photo.title}</h1>
+                                    <img src={photo.photoUrl} ></img>
                                 </li>
                             </Link>
-                            })               
-                        ): ("")
-                        }
-                    </div> */}
-
-
-                    {this.props.videos.length > 0 ? (
-                        <div className='result-container'>
-                            {
-                                this.props.videos.map(video => {
-                                return <Link to={`/videos/${video._id}`}>
-                                        <li className='content'>
-                                            <h1>{video.title}</h1>
-                                            <video controls >
-                                                <source src={video.videoUrl} type="video/mp4"></source>
-                                            </video>                     
-                                        </li>
-                                    </Link>
-                                    })               
-                                }
-                        </div>) : ("")
+                        })
                     }
-                    
-    
-                        {this.props.photos.length > 0 ? (
-                    <div className='result-container'>
-                            {this.props.photos.map(photo => {
-                                return <Link to={`/photos/${photo._id}`}>
-                                    <li className='content'>
-                                        <h1>{photo.title}</h1>
-                                        <img src={photo.photoUrl} ></img>
-                                    </li>
-                                </Link>
-                            })
-                        }
-                    </div>
-                        ): ("")
-                        }
-    
-                        {this.props.quotes.length > 0 ? (
-                    <div className='result-container'>
-                            {this.props.quotes.map(quote => {
-                                return <Link to={`/quotes/${quote._id}`}>
-                                    <li className='content'>
-                                        <h1>{quote.title}</h1> 
-                                        <blockquote>{quote.quoteText}</blockquote>
-                                    </li>
-                                </Link>
-                            })
-                        }
-                    </div>
-                        ): ("")
-                        }
-    
                 </div>
-                
-            )
-        
+                    ): ("")
+                }
+
+                {this.props.quotes.length > 0 ? (
+                    <div className='result-container'>
+                        {this.props.quotes.map(quote => {
+                            return <Link to={`/quotes/${quote._id}`} key={quote._id}>
+                                <li className='content' >
+                                    <h1>{quote.title}</h1> 
+                                    <blockquote>{quote.quoteText}</blockquote>
+                                </li>
+                            </Link>
+                        })
+                    }
+                </div>
+                    ): ("")
+                }
+
+            </div>    
+        )
     }
 }
 
