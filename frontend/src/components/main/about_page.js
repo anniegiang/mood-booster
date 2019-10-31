319855fe3f52b5757c833cab5252cf604776f330
Annie Giang <annieg4123@gmail.com>
2019-10-31 14:01:00 -0700

    Remove width in hover that caused elements to move

--- frontend/src/components/main/about_page.js
+++ frontend/src/components/main/about_page.js
@@ -2,9 +2,8 @@ import React from "react";
 import "./about_page.css";
 
 class AboutPage extends React.Component {
-
   handleClick() {
-    this.props.history.push({ pathname: '/home' })
+    this.props.history.push({ pathname: "/home" });
   }
 
   render() {
@@ -12,45 +11,55 @@ class AboutPage extends React.Component {
       <div className="about-container">
         <div className="about-window">
           <h2>Welcome to</h2>
-          
+
           <div className="logo-main"></div>
 
-          <p>Being in tech can be a grueling experience where individuals learn a lot, but still feel like they don't understand anything. 
-            Drawing from our experiences, we learned that productivity is directly correlated to an individual's mood, and so we've set out 
-            to create a way to boost that mood by providing content tailored to what you are experiencing now. Click the button below to check it out, 
-            or check out the profiles of the creators!</p>
-          
+          <p>
+            Being in tech can be a grueling experience where individuals learn a
+            lot, but still feel like they don't understand anything. Drawing
+            from our experiences, we learned that productivity is directly
+            correlated to an individual's mood, and so we've set out to create a
+            way to boost that mood by providing content tailored to what you are
+            experiencing now. Click the button below to check it out, or check
+            out the profiles of the creators!
+          </p>
+
           <br></br>
-          <button className="main_button" onClick={this.handleClick.bind(this)}>Home Page</button>
-        
-            
-            <div className="creators">
-              <h4>Creators:</h4>     
-                <div className="dropdown">
-                    <button className="dropbtn">Annie</button>
-                    <div className="dropdown-content">
-                        {/* <a href="#">Personal Site</a> */}
-                        <a href="https://github.com/anniegiang">GitHub</a>
-                        <a href="https://www.linkedin.com/in/annie-giang-113bb4101/">LinkedIn</a>
-                    </div>
-                </div>
-                <div className="dropdown">
-                    <button className="dropbtn">Johnson</button>
-                    <div className="dropdown-content">
-                        {/* <a href="#">Personal Site</a> */}
-                        <a href="https://github.com/joonson22">GitHub</a>
-                        <a href="https://www.linkedin.com/in/jdlai/">LinkedIn</a>
-                    </div>
-                </div>
-                <div className="dropdown">
-                    <button className="dropbtn">Rebecca</button>
-                    <div className="dropdown-content">
-                        {/* <a href="#">Personal Site</a> */}
-                        <a href="https://github.com/rebeccalambert">GitHub</a>
-                        <a href="https://www.linkedin.com/in/rebeccajlambert/">LinkedIn</a>
-                    </div>
-                </div>
+          <button className="main_button" onClick={this.handleClick.bind(this)}>
+            Home Page
+          </button>
+
+          <div className="creators">
+            <h4>Creators:</h4>
+            <div className="dropdown">
+              <button className="dropbtn">Annie</button>
+              <div className="dropdown-content">
+                {/* <a href="#">Personal Site</a> */}
+                <a href="https://github.com/anniegiang">GitHub</a>
+                <a href="https://www.linkedin.com/in/annie-giang-113bb4101/">
+                  LinkedIn
+                </a>
+              </div>
             </div>
+            <div className="dropdown">
+              <button className="dropbtn">Johnson</button>
+              <div className="dropdown-content">
+                {/* <a href="#">Personal Site</a> */}
+                <a href="https://github.com/joonson22">GitHub</a>
+                <a href="https://www.linkedin.com/in/jdlai/">LinkedIn</a>
+              </div>
+            </div>
+            <div className="dropdown">
+              <button className="dropbtn">Rebecca</button>
+              <div className="dropdown-content">
+                {/* <a href="#">Personal Site</a> */}
+                <a href="https://github.com/rebeccalambert">GitHub</a>
+                <a href="https://www.linkedin.com/in/rebeccajlambert/">
+                  LinkedIn
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
